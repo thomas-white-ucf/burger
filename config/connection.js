a6c6ae31dd834fca2002f1060d80ba10e16f7fca
Thomas White <thomas.white.ucf@gmail.com>
2019-09-19 16:46:18 -0400

    connection modified

--- config/connection.js
+++ config/connection.js
@@ -17,7 +17,7 @@ if (process.env.JAWSDB_URL) {
 } else {
   connection = mysql.createConnection({
       host: "localhost",
-      port: 3306,
+      // port: 3306,
       user: "root	",
       password: keys.databaseKey,
       database: "burgers_db"
