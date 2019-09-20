4383404893978848f6d119f0516b662b632ea646
Thomas White <thomas.white.ucf@gmail.com>
2019-09-19 23:18:50 -0400

    views for burgers

--- config/connection.js
+++ config/connection.js
@@ -15,6 +15,7 @@ var connection;
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
+  // database is local
   connection = mysql.createConnection({
       host: "localhost",
       port: 3306,
